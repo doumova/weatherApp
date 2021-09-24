bc9ca4280c80afcbbbda130706ed4449f7e3204e
doumova <doumogueye.mg@gmail.com>
2021-09-24 21:43:44 +0100

    deploying the app in heroku

--- app.js
+++ app.js
@@ -65,6 +65,6 @@ app.post("/", function(req,res){
 
 
 
-app.listen(5000, function(){
-    console.log("app is running on port 5000.");
+app.listen(process.env.PORT || 3000, function(){
+    console.log("app is running");
 });
